c66429db83e52c93829284dee872bf39955de545
김민관 <alsrhks2508@naver.com>
2023-03-17 03:08:45 +0900

    전성훈님 작업해야될 내용

---
+++ Main_Project/main/Server/server/tcp_communicate.py
@@ -0,0 +1,19 @@
+#초기값 설정 및 통신 세팅 함수
+def server():
+    return conn, addr
+
+
+# while 문 안에서 돌아갈 frame 받아오는 함수
+def get_stream(conn):
+    #작성 요망, 상수는 constant에 작성
+
+    return frame
+
+# 명령 전송할 함수 
+# command 인덱스 
+# 0 : nothing, 1 : next, 2 : previous, 3 : up, 4 : down, 5 : shutdown
+# 6 : turn on  7 : others 
+
+def send(conn, command):
+
+    
