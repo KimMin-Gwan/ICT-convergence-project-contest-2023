f281d8fc1d093322141f8350f2875d185c72cc30
김민관 <alsrhks2508@naver.com>
2023-03-17 11:59:16 +0900

    Update demo_client.py

---
+++ Main_Project/main/Raspberry_Pi/demo_client.py
@@ -0,0 +1,28 @@
+# >> pseudo code << 
+# procedure of main() function in server
+
+# 초기값 값 설정
+# 서버 초기값 설정
+# server.argument()
+# IR 센서 초기값 설정
+#
+# 본문 반복문 시작
+# while True:
+# 서버로 데이터 전송
+# sever.send()
+#
+# 커멘드 수신
+# command = sever.get()
+# 커멘드가 0번인지 확인
+# if command is Not 0:
+# continue
+#
+# 아니면 커멘드 실행
+# else:
+# utils.operater(command)
+# 상태에 따라 led켜기
+#
+# 
+통신이 끊어지 않는한 무한 반복
+#
+# end of procedure
