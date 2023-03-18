fcf6d6094b8e19a6066930887081659aa4937cf7
김민관 <alsrhks2508@naver.com>
2023-03-18 17:11:28 +0900

    TCP 서버

--- Main_Project/main/Server/server/tcp_communicate.py
+++ Main_Project/main/Server/server/tcp_communicate.py
@@ -1,19 +1,52 @@
-#초기값 설정 및 통신 세팅 함수
+import socket
+import cv2
+import numpy as np
+import server
+ 
+#socket에서 수신한 버퍼를 반환하는 함수
+def recvall(sock, count):
+    # 바이트 문자열
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+ 
 def server():
-    return conn, addr
+    #Ip, PORT 설정
 
+    #TCP 사용
+    server_sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print('Socket created')
+    
+    #서버의 아이피와 포트번호 지정
+    #s.bind((HOST,PORT))
+    server_sock.bind((server.HOST, server.PORT))
+    print('Socket bind complete')
+    # 클라이언트의 접속을 기다린다. (클라이언트 연결을 10개까지 받는다)
+    server_sock.listen(10)
+    print('Socket now listening')
+    
+    #연결, conn에는 소켓 객체, addr은 소켓에 바인드 된 주소
+    conn,addr=server_sock.accept()
 
-# while 문 안에서 돌아갈 frame 받아오는 함수
+    return conn,addr
+ 
 def get_stream(conn):
-    #작성 요망, 상수는 constant에 작성
+    # client에서 받은 stringData의 크기 (==(str(len(stringData))).encode().ljust(16))
+    length = recvall(conn, 16)
+    stringData = recvall(conn, int(length))
+    data = np.fromstring(stringData, dtype = 'uint8')
+    
+    #data를 디코딩한다.
+    frame = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    #cv2.imshow('ImageWindow',frame)
+    #cv2.waitKey(1)
 
     return frame
 
-# 명령 전송할 함수 
-# command 인덱스 
-# 0 : nothing, 1 : next, 2 : previous, 3 : up, 4 : down, 5 : shutdown
-# 6 : turn on  7 : others 
-
 def send(conn, command):
-
-    
+    #client로 데이터 보내기 테스트
+    conn.send(command.encode())
