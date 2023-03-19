6e9862965483c682a4114198a3c394c8d991c5b6
bo-kyung <127110130+qhrud02@users.noreply.github.com>
2023-03-19 16:54:24 +0900

    add def

--- Main_Project/main/Raspberry_Pi/demo_client.py
+++ Main_Project/main/Raspberry_Pi/demo_client.py
@@ -5,24 +5,30 @@ import utils
 # 초기값 값 설정
 # 서버 초기값 설정
 # IR 센서 초기값 설정
-#
-# 본문 반복문 시작
-# while True:
-# 서버로 데이터 전송
-# sever.send()
-#
-# 커멘드 수신
-# command = sever.get()
-# 커멘드가 0번인지 확인
-# if command is Not 0:
-# continue
-#
-# 아니면 커멘드 실행
-# else:
-# utils.operater(command)
-# 상태에 따라 led켜기
-#
-# 
+server.argument()
+IR_sensor.setup()
+
+# 초록색 LED를 켜는 함수(프로그램 동작)
+def turn_on_green_LED():
+    IR_sensor.turn_on_LED("green")
+    IR_sensor.turn_off_LED("red")
+    IR_sensor.turn_off_LED("blue")
+
+# 파란색 LED를 켜는 함수(커멘드가 0이 아닐경우)
+def turn_on_blue_LED():
+    IR_sensor.turn_off_LED("green")
+    IR_sensor.turn_off_LED("red")
+    IR_sensor.turn_on_LED("blue")
+
+# 빨간색 LED를 켜는 함수 (커멘드가 0일 경우)
+def turn_on_red_LED():
+    IR_sensor.turn_off_LED("green")
+    IR_sensor.turn_on_LED("red")
+    IR_sensor.turn_off_LED("blue")
+
+# 기본 상태를 초록색 LED가 켜짐으로써 확인하기
+turn_on_green_LED()
+
 # 통신이 끊어지 않는한 무한 반복
 while True:
     # 서버로 데이터 전송하기
