37811956d9be4378454bcb8d2cf6d5f79ccd554b
Vespoi <103725786+Vespoi@users.noreply.github.com>
2023-03-20 21:54:38 +0900

    constant update

--- Main_Project/main/Raspberry_Pi/IR_trans/IR_sensor.py
+++ Main_Project/main/Raspberry_Pi/IR_trans/IR_sensor.py
@@ -1,21 +1,22 @@
 import os
+import IR_trans
 
 #tv전원 on/off 기능
 def tv_power():
-    os.system('irsend -3 # SEND_ONCE wow4 KEY_POWER')   #신호를 한 번 보낼 때 여러번 보냄 
+    os.system(IR_trans.power)   #신호를 한 번 보낼 때 여러번 보냄 
 
 #채널업
 def tv_channelup():
-    os.system('irsend SEND_ONCE wow4 KEY_CHANNELUP')
+    os.system(IR_trans.ch_up)
 
 #채널 다운
 def tv_channeldown():
-    os.system('irsend SEND_ONCE wow4 KEY_CHANNELDOWN')
+    os.system(IR_trans.ch_down)
 
 #소리 줄임
 def tv_volumedown():
-    os.system('irsend SEND_ONCE wow4 KEY_VOLUMEDOWN')
+    os.system(IR_trans.vol_down)
 
 #소리 키움
 def tv_volumeup():
-    os.system('irsend SEND_ONCE wow4 KEY_VOLUMEUP')
+    os.system(IR_trans.vol_up)
